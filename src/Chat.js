4d8f60ceafcfba9173a60d976bc0d5fc3fc80f23
asep10001 <asep10001@mail.unpad.ac.id>
2021-01-01 19:46:24 +0700

    adding sidebar and sidebar chat

--- src/Chat.js
+++ src/Chat.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import './chat.css'
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import React from "react";
+import "./chat.css";
 
 function Chat() {
-    return (
-        <div className="chat">
-            <h1>I am the chat</h1>
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at ..</p>
         </div>
-    )
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
