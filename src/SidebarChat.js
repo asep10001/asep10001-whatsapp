de9339cfc32a16c65b509b5df724d7dc8c62ccfb
asep10001 <asep10001@mail.unpad.ac.id>
2021-01-01 19:27:27 +0700

    init

---
+++ src/SidebarChat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SidebarCaht() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SidebarCaht
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDMPE4WdYsTujb5AkUQEqeYPPUEVhRuj6E",
+    authDomain: "asep10001-whatsapp.firebaseapp.com",
+    projectId: "asep10001-whatsapp",
+    storageBucket: "asep10001-whatsapp.appspot.com",
+    messagingSenderId: "794113925774",
+    appId: "1:794113925774:web:8e42f75661d276cbaddfdf"
+  };
